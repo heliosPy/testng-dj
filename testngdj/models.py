959b738c27bc97e55db06f1cbbcf0b34820f66c5
heliosPy <63154000+heliosPy@users.noreply.github.com>
2021-04-03 05:10:39 +0530

    Commited from window

--- testngdj/models.py
+++ testngdj/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=250)
     age = models.IntegerField()
+
+
+class Parent(models.Model):
+    name = models.CharField(db_column='ParentName', max_length=200)
+    modified = models.DateTimeField(db_column='Modified_on')
+    child = models.ForeignKey('Student', models.DO_NOTHING)
