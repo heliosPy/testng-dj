ec64144d205c16fd0c88302b3e1790fef603f8b6
helios <helios2844@gmail.com>
2021-04-04 22:08:27 +0530

    new commit from linux

--- testngdj/models.py
+++ testngdj/models.py
@@ -8,5 +8,5 @@ class Student(models.Model):
 
 class Parent(models.Model):
     name = models.CharField(db_column='ParentName', max_length=200)
-    modified = models.DateTimeField(db_column='Modified_on')
     child = models.ForeignKey('Student', models.DO_NOTHING)
+    age = models.CharField(db_column='Age', max_length=10)
